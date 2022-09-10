14fa281197d7813b02e159b4ddf4d668ec080ab1
Rene Hanslin <rene.hanslin@hseag.com>
2022-09-10 15:07:44 +0200

    ESLint & Prettier running

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// see https://eslint.vuejs.org/user-guide/#usage
+
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    "vue/require-default-prop": "off",
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+var x = "hello";
+console.log(x);
+createApp(App).mount("#app");
